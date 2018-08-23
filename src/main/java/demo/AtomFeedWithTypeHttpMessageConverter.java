559481ec67cd4fd2b1d24b5aa78f3260239bb2d8
Mark Hobson <markhobson@gmail.com>
2018-08-23 19:51:35 +0100

    Add workaround to get Atom feed with type parameter

---
+++ src/main/java/demo/AtomFeedWithTypeHttpMessageConverter.java
@@ -0,0 +1,64 @@
+package demo;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.http.converter.feed.AtomFeedHttpMessageConverter;
+import org.springframework.util.StringUtils;
+
+import com.rometools.rome.feed.atom.Feed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.WireFeedOutput;
+
+import static java.util.Collections.singletonList;
+
+/**
+ * Spring HTTP message converter for an Atom feed.
+ */
+public class AtomFeedWithTypeHttpMessageConverter extends AtomFeedHttpMessageConverter
+{
+	public AtomFeedWithTypeHttpMessageConverter()
+	{
+		setSupportedMediaTypes(singletonList(MediaType.valueOf("application/atom+xml;type=feed")));
+	}
+
+	// Workaround: https://jira.spring.io/browse/SPR-17040
+	// modified from AbstractWireFeedHttpMessageConverter.writeInternal
+	@Override
+	protected void writeInternal(Feed wireFeed, HttpOutputMessage outputMessage) throws IOException,
+		HttpMessageNotWritableException
+	{
+		String wireFeedEncoding = wireFeed.getEncoding();
+		
+		if (!StringUtils.hasLength(wireFeedEncoding))
+		{
+			wireFeedEncoding = DEFAULT_CHARSET.name();
+		}
+		
+		MediaType contentType = outputMessage.getHeaders().getContentType();
+		
+		if (contentType != null)
+		{
+			Charset wireFeedCharset = Charset.forName(wireFeedEncoding);
+//			contentType = new MediaType(contentType.getType(), contentType.getSubtype(), wireFeedCharset);
+			contentType = new MediaType(contentType, wireFeedCharset);
+			outputMessage.getHeaders().setContentType(contentType);
+		}
+
+		WireFeedOutput feedOutput = new WireFeedOutput();
+		try
+		{
+			Writer writer = new OutputStreamWriter(outputMessage.getBody(), wireFeedEncoding);
+			feedOutput.output(wireFeed, writer);
+		}
+		catch (FeedException ex)
+		{
+			throw new HttpMessageNotWritableException("Could not write WireFeed: " + ex.getMessage(), ex);
+		}
+	}
+}
--- src/main/java/demo/WebMvcConfig.java
+++ src/main/java/demo/WebMvcConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.feed.AtomFeedHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,6 +13,14 @@ public class WebMvcConfig implements WebMvcConfigurer
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters)
 	{
+		// Replace default Atom feed message converter with one that can add 'type' parameter to Content-Type header
+		converters.stream()
+			.filter(converter -> converter instanceof AtomFeedHttpMessageConverter)
+			.findFirst()
+			.map(converters::remove);
+		converters.add(new AtomFeedWithTypeHttpMessageConverter());
+		
+		// Add new Atom entry message converter
 		converters.add(new AtomEntryHttpMessageConverter());
 	}
 }
